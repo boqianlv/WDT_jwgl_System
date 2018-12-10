475fa66435701376ebf037090eb38f5e8ee64acb
boqianlv <3049344036@qq.com>
2018-12-10 18:27:35 +0800

    Delete config.php

--- login/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbms='mysql';
-$host='localhost';
-$dbname='wdt_jwgl';
-$user='root';
-$pass='lbq2018980511';
-$dsn="$dbms:host=$host;dbname=$dbname";
-header('Access-Control-Allow-Origin: *');
